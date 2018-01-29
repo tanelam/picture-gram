885786958f5f0a6d79f83ff953d2588babc7e34b
tanelam <tania.e.aparicio@gmail.com>
2018-01-29 15:31:17 -0500

    user table was created and validations were added to the users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
+  has_secure_password
 
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :username, uniqueness: true
+  validates :username, presence: true
 end
